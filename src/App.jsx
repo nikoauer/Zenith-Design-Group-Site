ead8b06d71a0e4fef087d25abab1487e9e96d559
Niko Auer <nikoauer26@gmail.com>
2024-03-02 15:23:12 +1100

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Niko + Auer</h1>
+      <h1>Niko + Auer test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
